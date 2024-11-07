5b7e67eb3410e96c18023a482731dc3d885844cf
NxPKG js <iconmamundentist@gmail.com>
2024-11-07 03:06:03 +0000

    fix/debug

---
+++ test-artifact-id/src/main/java/foo/test/entities/UserToGroup.java
@@ -0,0 +1,18 @@
+package foo.test.entities;
+
+import com.khulnasoft.data.*;
+import com.khulnasoft.data.annotation.*;
+
+/**
+ * Represents a user group relation for the application.
+ */
+@Relation
+public class UserToGroup
+  implements EntityRelationDescriptor<User, Group>
+{
+    @Left
+    User userId;
+
+    @Right
+    Group groupId;
+}
---
+++ test-artifact-id/src/main/java/foo/test/handlers/HomeHandler.java
@@ -0,0 +1,20 @@
+package foo.test.handlers;
+
+import com.khulnasoft.gemini.*;
+import com.khulnasoft.gemini.path.*;
+import com.khulnasoft.gemini.path.annotation.*;
+
+public class HomeHandler extends MethodUriHandler<Context>
+{
+
+  public HomeHandler(GeminiApplication app)
+  {
+    super(app);
+  }
+
+  @Path
+  @Get
+  public boolean root() throws Exception {
+    return json();
+  }
+}
